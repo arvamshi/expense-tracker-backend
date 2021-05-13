a08bc2e7c12a9735ed2c5737d19e393b089fb435
Rithwik Vamshi <rithwikvamshirv@gmail.comi>
2021-05-13 20:55:14 +0530

    .

--- src/main/java/com/example/Test1/controller/ExpenseController.java
+++ src/main/java/com/example/Test1/controller/ExpenseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 @RestController
 public class ExpenseController {
